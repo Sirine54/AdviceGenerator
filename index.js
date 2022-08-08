5dedf71de64a025ad8bae8d0cdd79d77fc95279c
Sirine Saoudi <95916760+Sirine54@users.noreply.github.com>
2022-08-09 01:52:35 +0200

    challenge done

---
+++ index.js
@@ -0,0 +1,20 @@
+
+/////DOM ////////////
+const num = document.getElementById('number');
+const btn = document.getElementById('btn');
+const quote= document.getElementById('quote');
+
+function adviceGenerator(){
+fetch('https://api.adviceslip.com/advice')
+.then(res=>res.json())
+.then(data=>{
+    console.log(data.slip.advice)
+    quote.innerHTML=data.slip.advice;
+    console.log(data.slip.id)
+    num.innerHTML=data.slip.id;
+})
+}
+
+adviceGenerator()
+
+btn.addEventListener('click',adviceGenerator);
